47869c57e0cc629254b5247b0bb9700b83866db9
Daniel <danielgsoftware@gmail.com>
2019-11-21 18:03:29 +0100

    Added spacebar validation for password

--- Fit-planner/app/Services/Auth/UserValidationService.php
+++ Fit-planner/app/Services/Auth/UserValidationService.php
@@ -22,7 +22,7 @@ class UserValidationService
         return $this->validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'password' => ['required', 'string', 'min:8', 'confirmed', 'regex:/^\S*$/u'],
         ])->validate();
     }
 }
