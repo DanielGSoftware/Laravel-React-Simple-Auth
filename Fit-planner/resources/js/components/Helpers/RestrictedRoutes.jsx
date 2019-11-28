66b05f93fc6fc6ec8a0a3b3dc48dd1d2c5366200
Daniel <danielgsoftware@gmail.com>
2019-11-28 15:43:05 +0100

    Formatted

--- Fit-planner/resources/js/components/Helpers/RestrictedRoutes.jsx
+++ Fit-planner/resources/js/components/Helpers/RestrictedRoutes.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
-import { isLogin } from './Utilities';
+import {Route, Redirect} from 'react-router-dom';
+import {isLogin} from './Utilities';
 
 export const PrivateRoute = ({component: Component, ...rest}) => {
     return (
         <Route {...rest} render={props => (
             isLogin() ?
                 <Component {...props} />
-                : <Redirect to="/login" />
-        )} />
+                : <Redirect to="/login"/>
+        )}/>
     );
 };
 
-export const GuestRoute = ({component: Component,  ...rest}) => {
+export const GuestRoute = ({component: Component, ...rest}) => {
     return (
         <Route {...rest} render={props => (
             isLogin() ?
-                <Redirect to="/" />
+                <Redirect to="/"/>
                 : <Component {...props} />
-        )} />
+        )}/>
     );
 };
